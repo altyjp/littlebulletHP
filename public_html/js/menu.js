8c3a2a65b283d1039e26492ca922ee66f1bb985e
satouryou <satouryou@192.168.0.2>
2017-11-18 10:00:04 +0900

    jsのシンタックスエラーを解消

--- public_html/js/menu.js
+++ public_html/js/menu.js
@@ -19,25 +19,25 @@ function window_load() {
         var $display_size;
 	$display_size = window.innerWidth;
         //初回時
-        if($display_mode == undefined){
+        if($display_mode === undefined){
             if($display_size > 768){
                 $display_mode = "PC";
             } else {
-                $display_mode = "SP"
+                $display_mode = "SP";
             }
             return;
         //2回目以降 SPモードからPCモードになった場合、画面を強制リフレッシュ
         } else {
-            if ($display_mode == "SP"){
+            if ($display_mode === "SP"){
                 if($display_size > 768){
-                    location.reload()
+                    location.reload();
                 }
         // そうでない場合は現在の状態を観測する
             } else {
                 if($display_size > 768){
                     $display_mode = "PC";
                 } else {
-                    $display_mode = "SP"
+                    $display_mode = "SP";
                 }
             }
         }
@@ -48,10 +48,10 @@ function menu_btn(){
     
     var $display = document.getElementById("nav").style.display;
     
-    if($display == "none" || $display == ""){
+    if($display === "none" || $display === ""){
         //表示処理
         document.getElementById("nav").style.display = "block";
-        document.getElementById("headerMenu").style.height = "370px";
+        document.getElementById("headerMenu").style.height = "400px";
     } else {
         //非表示処理
         document.getElementById("nav").style.display = "none";
